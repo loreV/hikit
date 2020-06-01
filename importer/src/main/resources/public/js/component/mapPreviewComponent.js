e97b9f2b91a8324393ecb961d04d8e7a9d2df3d1
loreV <lore.verri@gmail.com>
2020-06-01 13:58:16 +0200

    H0020 Fixed import visualization

--- importer/src/main/resources/public/js/component/mapPreviewComponent.js
+++ importer/src/main/resources/public/js/component/mapPreviewComponent.js
@@ -1,36 +1,39 @@
-var mapComponentPreview = Vue.component('map-preview', {
+var mapComponentPreview = Vue.component("map-preview", {
   props: {
-    points: Array
+    points: Array,
   },
   data: {
     map: null,
-    mapProjection: {
-      EPSG4326: "EPSG:4326",
-      EPSG3857: "EPSG:3857"
-    },
+    mapProjection: {},
     // Colors
-    mapStyling: {
-      black: "#000000"
-    },
-    processing: {
-      decimalApproximation: 5
-    }
+    mapStyling: {},
+    processing: {},
   },
   methods: {
     onUpload() {
       var points = [];
 
       for (var i = 0; i < this.points.length; i++) {
-        points[i] = [this.points[i].longitude.toFixed(this.processing.decimalApproximation),
-        this.points[i].latitude.toFixed(this.processing.decimalApproximation)];
+        points[i] = [
+          this.points[i].values[0].toFixed(
+            this.processing.decimalApproximation
+          ),
+          this.points[i].values[1].toFixed(
+            this.processing.decimalApproximation
+          ),
+        ];
       }
 
       for (var i = 0; i < points.length; i++) {
-        points[i] = ol.proj.transform(points[i], this.mapProjection.EPSG4326, this.mapProjection.EPSG3857);
+        points[i] = ol.proj.transform(
+          points[i],
+          this.mapProjection.EPSG4326,
+          this.mapProjection.EPSG3857
+        );
       }
 
       var featureLine = new ol.Feature({
-        geometry: new ol.geom.LineString(points)
+        geometry: new ol.geom.LineString(points),
       });
 
       var vectorLine = new ol.source.Vector({});
@@ -39,36 +42,48 @@ var mapComponentPreview = Vue.component('map-preview', {
       var vectorLineLayer = new ol.layer.Vector({
         source: vectorLine,
         style: new ol.style.Style({
-          fill: new ol.style.Fill({ color: '#00FF00', weight: 4 }),
-          stroke: new ol.style.Stroke({ color: this.mapStyling.black, width: 5 })
-        })
+          fill: new ol.style.Fill({ color: "#00FF00", weight: 4 }),
+          stroke: new ol.style.Stroke({
+            color: this.mapStyling.black,
+            width: 5,
+          }),
+        }),
       });
 
-      this.view.setZoom(15)
+      this.view.setZoom(15);
       this.view.setCenter(points[0]);
       this.map.addLayer(vectorLineLayer);
-    }
+    },
   },
   watch: {
-    'points': function () {
+    points: function () {
       this.onUpload();
-    }
+    },
   },
   mounted: function () {
     this.view = new ol.View({
       center: ol.proj.fromLonLat([11.365946, 44.513743]),
-      zoom: 6
+      zoom: 6,
     });
     this.map = new ol.Map({
-      target: 'map',
+      target: "map",
       layers: [
         new ol.layer.Tile({
-          source: new ol.source.OSM()
-        })
+          source: new ol.source.OSM(),
+        }),
       ],
-      view: this.view
+      view: this.view,
     });
-
+    this.processing = {
+      decimalApproximation: 5,
+    };
+    this.mapProjection = {
+      EPSG4326: "EPSG:4326",
+      EPSG3857: "EPSG:3857",
+    };
+    this.mapStyling = {
+      black: "#000000",
+    };
   },
-  template: '<div id="map" class="map"></div>'
+  template: '<div id="map" class="map"></div>',
 });
--- importer/src/main/resources/public/js/component/uploadComponent.js
+++ importer/src/main/resources/public/js/component/uploadComponent.js
@@ -10,15 +10,15 @@ var uploadInputComponent = Vue.component('upload-input', {
           'Content-Type': 'multipart/form-data'
         }
       }).then(response => (this.$emit('uploadResponse', response)))
-
-      axios.post(BASE_API_ADDRESS_SERVICE + "/api/v1/trails/", customFormData, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(response => (this.$emit('uploadConnectionResponse', response)))
+//      axios.post(BASE_API_ADDRESS_SERVICE + "/api/v1/trails", customFormData, {
+//        headers: {
+//          'Content-Type': 'application/json'
+//        }
+//      }).then(response => (this.$emit('uploadConnectionResponse', response)))
     }
   },
   template: `
+
   <form v-on:change="readGpxFile">
     <div class="file">
         <label class="file-label">
